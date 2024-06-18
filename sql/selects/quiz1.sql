07b0b3b0ab71d518e7ab476c5d998557676a1bf6
madonuko <madonuko@outlook.com>
2024-06-18 23:25:19 +0800

    feat(sql/selects): add .sql file

---
+++ sql/selects/quiz1.sql
@@ -0,0 +1,10 @@
+CREATE TABLE students (
+	id INTEGER PRIMARY KEY,
+	ename TEXT NOT NULL,
+	joinyear INTEGER
+);
+
+INSERT INTO students (ename, joinyear) VALUES
+	('Chan Tai Man', 2020),
+	('Cheung Siu Mei', 2019),
+	('Fung Yat Sum', 2019);
